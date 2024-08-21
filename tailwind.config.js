245cc653213436db4f260ae6dd7d9d4da7c3dc31
StackMastery <labib.developers@gmail.com>
2024-08-21 22:56:47 +0600

    Header Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,35 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'primary': '1440px',
+      },
+      colors: {
+        'primary': '#ABEF5F',
+        'lightPrimary': 'rgba(171, 239, 95, 0.5)',
+        'textcolor': '#212121',
+        'borderColor': '#E9E9E9'
+      },
+      fontFamily: {
+        'main': 'Manrope'
+      },
+      screens: {
+        '3xl': '1536px', 
+        '2xl': '1440px',
+        'xl': '1280px',
+        'lg': '1024px',
+        'md': '868px',
+        'sm': '640px',
+        'sx': '567px',
+        'xs': '480px',
+        '2xs': '375px',
+        '3xs': '320px',
+        '4xs': '250px'  
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
-
